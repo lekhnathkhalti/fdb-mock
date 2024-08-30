e24594ade1b720b630c3f4aedf69754a67b58dcb
Lekh Nath Khanal <lekhnath@sageflick.com>
2024-08-30 15:19:16 +0545

    introduces the header dependecies in api

---
+++ app/lib/header_checker.py
@@ -0,0 +1,10 @@
+from fastapi import Header, HTTPException
+
+
+async def check_custom_headers(
+    Username: str = Header(None),
+    Password: str = Header(None),
+    TheaderCode: str = Header(None),
+):
+    if not all([Username, Password, TheaderCode]):
+        HTTPException(status_code=400, detail="Required header parameters are missing.")
--- app/main.py
+++ app/main.py
@@ -1,11 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
+from app.lib.header_checker import check_custom_headers
 from app.routers import theater, ticket, return_ticket, show, cancel_show
 
 app = FastAPI()
 
-app.include_router(theater.router, tags=["Theater"])
-app.include_router(ticket.router, tags=["Ticket"])
-app.include_router(return_ticket.router, tags=["Ticket Return"])
-app.include_router(show.router, tags=["Show"])
-app.include_router(cancel_show.router, tags=["Ticket Cancel"])
+app.include_router(
+    theater.router, tags=["Theater"], dependencies=[Depends(check_custom_headers)]
+)
+app.include_router(
+    ticket.router, tags=["Ticket"], dependencies=[Depends(check_custom_headers)]
+)
+app.include_router(
+    return_ticket.router,
+    tags=["Ticket Return"],
+    dependencies=[Depends(check_custom_headers)],
+)
+app.include_router(
+    show.router, tags=["Show"], dependencies=[Depends(check_custom_headers)]
+)
+app.include_router(
+    cancel_show.router,
+    tags=["Ticket Cancel"],
+    dependencies=[Depends(check_custom_headers)],
+)
