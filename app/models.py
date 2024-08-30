d9c274f5f9655c30c6d2fa4bec4b9e4c456046ed
Lekh Nath Khanal <lekhnath@sageflick.com>
2024-08-30 11:12:32 +0545

    updates model structure

--- app/models.py
+++ app/models.py
@@ -1,75 +1,139 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Optional
+from datetime import datetime
 
-class TheaterData(BaseModel):
-    theater_code: str
-    screen_name: str
-    screen_screened: Optional[str]
-    screen_fiscal_year: Optional[str]
-    screen_capacity: Optional[int]
-    screen_rows: Optional[int]
-    screen_columns: Optional[int]
-    screen_vertical_start: Optional[int]
-    screen_horizontal_start: Optional[int]
-    screen_disable_seats: Optional[int]
-    screen_ticket: Optional[str]
+
+class Genre(BaseModel):
+    Name: str
+    GenreCode: str
+
+
+class Director(BaseModel):
+    Name: str
+    DirectorCode: str
+
+
+class Producer(BaseModel):
+    Name: str
+    ProducerCode: str
+
+
+class Distributor(BaseModel):
+    Name: str
+    DistributorCode: str
+
+
+class MovieInfo(BaseModel):
+    SerialNumber: str
+    MovieCode: str
+    MovieName: str
+    Grade: str
+    ProductionHouse: str
+    Language: str
+    ProductionType: int
+    Genres: List[Genre]
+    Directors: List[Director]
+    Producers: List[Producer]
+    Distributors: List[Distributor]
+    ReleaseDate: datetime
+
+
+class TicketType(BaseModel):
+    Name: str
+    Seats: List[str]
+
+
+class TheaterDetails(BaseModel):
+    Name: str
+    ScreenId: int
+    FiscalYear: str
+    Capacity: int
+    Rows: int
+    Columns: int
+    VerticalStart: str
+    HorizontalStart: str
+    DisableSeats: List[str]
+    TicketType: List[TicketType]
+
+
+class Tax(BaseModel):
+    Title: str
+    Rate: float
+    Value: float
+    Unit: int
+
+
+class Charge(BaseModel):
+    Title: str
+    Rate: float
+    Value: float
+    Unit: int
+
 
 class TicketDetails(BaseModel):
-    theater_code: str
-    screen_name: str
-    screened: Optional[str]
-    show_type_name: Optional[str]
-    show_typed: Optional[str]
-    movie_name: Optional[str]
-    movie_code: Optional[str]
-    fiscal_year: Optional[str]
-    show_date_time: Optional[str]
-    showed: Optional[str]
-    ticket_print_date_time: Optional[str]
-    ticket_type_name: Optional[str]
-    ticket_typed: Optional[str]
-    payment_type_name: Optional[str]
-    payment_typed: Optional[str]
-    ticket_code: Optional[str]
-    seat_number: Optional[str]
-    ticket_status_name: Optional[str]
-    ticket_status_value: Optional[str]
-    ticket_net_price: Optional[float]
-    ticket_price: Optional[float]
-    ticket_tax: Optional[float]
-    ticket_charge: Optional[float]
-    distributor_code: Optional[str]
-    distributor_commission_value: Optional[float]
-    is_real_time: Optional[bool]
+    TheaterName: str
+    TheaterCode: str
+    ScreenName: str
+    ScreenId: int
+    ShowTypeName: str
+    ShowTypeId: int
+    MovieName: str
+    MovieCode: str
+    FiscalYear: str
+    ShowDateTime: datetime
+    ShowId: int
+    TicketPrintDateTime: datetime
+    TicketTypeName: str
+    TicketTypeId: int
+    PaymentTypeName: str
+    PaymentTypeId: int
+    TicketCode: str
+    SeatNumber: str
+    TicketStatusName: str
+    TicketStatusValue: int
+    TicketNetPrice: float
+    TicketPrice: float
+    TicketTax: List[Tax]
+    TicketCharge: List[Charge]
+    DistributorCode: str
+    DistributorCommissionValue: float
+    IsRealTime: int
 
-class ReturnTicketDetails(BaseModel):
-    show_date_time: str
-    ref_ticket_code: str
-    ticket_cancelled_reason: Optional[str]
-    ticket_cancelled_date_time: Optional[str]
-    ticket_net_price: Optional[float]
-    ticket_price: Optional[float]
-    ticket_tax: Optional[float]
-    ticket_charge: Optional[float]
-    is_real_time: Optional[bool]
-    theater_code: str
-    fiscal_year: Optional[str]
-    showed: Optional[str]
+
+class TicketReturn(BaseModel):
+    TheaterCode: str
+    FiscalYear: str
+    ShowId: int
+    ShowDateTime: datetime
+    RefTicketCode: str
+    TicketCancelledReason: str
+    TicketCancelledDateTime: datetime
+    TicketNetPrice: float
+    TicketPrice: float
+    TicketTax: List[Tax]
+    TicketCharge: List[Charge]
+    IsRealTime: int
+
 
 class ShowDetails(BaseModel):
-    theater_code: str
-    fiscal_year: Optional[str]
-    showed: Optional[str]
-    show_status: Optional[str]
-    show_date_time: Optional[str]
-    show_added_date_time: Optional[str]
-    is_real_time: Optional[bool]
+    TheaterCode: str
+    FiscalYear: str
+    ShowId: int
+    ShowStatus: int
+    ShowDateTime: datetime
+    ShowAddedDateTime: datetime
+    IsRealTime: int
+
 
 class CancelShowDetails(BaseModel):
-    theater_code: str
-    fiscal_year: Optional[str]
-    showed: Optional[str]
-    show_date_time: Optional[str]
-    show_cancelled_reason: Optional[str]
-    show_cancelled_date_time: Optional[str]
-    is_real_time: Optional[bool]
+    TheaterCode: str
+    FiscalYear: str
+    ShowId: int
+    ShowDateTime: datetime
+    ShowCancelledReason: Optional[str] = Field(
+        default=None, description="Reason for show cancellation, if any"
+    )
+    ShowCancelledDateTime: Optional[datetime] = Field(
+        default=None, description="Date and time when the show was cancelled, if any"
+    )
+    IsRealTime: int
