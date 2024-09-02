837c127a28dddde1234e85f4007bd79c5e4a96f7
Lekh Nath Khanal <lekhnath@sageflick.com>
2024-09-02 10:33:07 +0545

    returns a dict object

--- app/lib/resp.py
+++ app/lib/resp.py
@@ -6,4 +6,4 @@ from fastapi.responses import JSONResponse
 
 
 def prepare_response(data: json):
-    return JSONResponse(content=jsonable_encoder(data))
+    return JSONResponse(content=jsonable_encoder({"data": data}))
