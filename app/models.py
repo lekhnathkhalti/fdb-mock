8f95eca70afc5a0ca7cbc265c06d93a88c978b8a
Lekh Nath Khanal <lekhnath@sageflick.com>
2024-09-02 17:30:41 +0545

    fields corrected.

--- app/models.py
+++ app/models.py
@@ -129,21 +129,6 @@ class CancelShowDetails(BaseModel):
     TheaterCode: str
     FiscalYear: str
     ShowId: int
-    ShowCancelledReason: Optional[str] = Field(
-        default=None, description="Reason for show cancellation, if any"
-    )
-    ShowCancelledDateTime: Optional[str] = Field(
-        default=None, description="Date and time when the show was cancelled, if any"
-    )
+    ShowCancelledReason: str
+    ShowCancelledDateTime: str
     IsRealTime: int
-
-
-{
-    "ShowId": 1,
-    "FiscalYear": "2024/2025",
-    "IsRealTime": 2,
-    "ShowStatus": 1,
-    "TheaterCode": "N/A",
-    "ShowCancelledReason": "This is show creations",
-    "ShowCancelledDateTime": "2024-09-02 11:19:07",
-}
