0070c5c77255b0b44e56b9f7fb059e3af23079ab
Lekh Nath Khanal <lekhnath@sageflick.com>
2024-09-05 17:40:17 +0545

    corrects the model name

--- app/routers/movie_info.py
+++ app/routers/movie_info.py
@@ -1,11 +1,11 @@
 from app.lib import resp
 from typing import List
 from fastapi import APIRouter
-from app.models import CancelShowDetails
+from app.models import MovieInfo
 
 router = APIRouter(prefix="/API/V1")
 
 
 @router.post("/MovieInfoList")
-async def movie_info(data: List[CancelShowDetails]):
+async def movie_info(data: List[MovieInfo]):
     return resp.prepare_response(data=data)
